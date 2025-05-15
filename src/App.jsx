03cd67bf66c213bb0b94d648f22c995ffc33e818
hhafzahh <2001231F@student.tp.edu.sg>
2025-05-15 17:33:42 +0800

    Added Necessary Components
    - installed tailwindcss
    - added a navbar and states to check if menu is open

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,20 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { Navbar } from './components/NavBar'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  //menuOpen - a boolean that tells whether the menu is open(t) or closed(f)
+  //setMenuOpen - a function u can use to change the value of menuOpen
+  //useState(f) - is basically initialising menuOpen to false = menu is closed by default
+  const [menuOpen, setMenuOpen] = useState(false);
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+      {/* passes 2 props to the component by passing the current state and passing the function to change it */}
+      <Navbar menuOpen = {menuOpen} setMenuOpen = {setMenuOpen}/>
     </>
   )
 }
