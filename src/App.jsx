f7a1a9cc248987fd5fa74b255a43a128a8d6cc38
hhafzahh <2001231F@student.tp.edu.sg>
2025-05-16 13:24:53 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import "./App.css";
-import { Navbar } from "./components/NavBar";
+import { Navbar } from "./components/Navbar";
 import { MobileMenu } from "./components/MobileMenu";
 import { Home } from "./components/sections/Home";
 import { About } from "./components/sections/About";
